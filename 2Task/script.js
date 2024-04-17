d8cc65134c7dd6f00f1bbafc5fc4756fa755f0ef
C R Sarath Kumar <165871282+Sarath0046@users.noreply.github.com>
2024-04-17 10:22:47 +0530

    Add files via upload

---
+++ 2Task/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings: ["An Inspiration", "Helper", "Feeds People", "Nobel price for peace", "Poor People's God", "Reformer",],
+    typeSpeed:120,
+    backSpeed:80,
+    loop:true
+});
